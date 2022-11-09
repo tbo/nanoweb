129ff57f305ad12835b0f4f86a15d4f700929288
Thomas Bonk <thomas@bonk.dev>
2022-11-09 21:46:04 +0100

    Chnage unsafe return type

--- packages/jsx/src/index.ts
+++ packages/jsx/src/index.ts
@@ -23,7 +23,7 @@ const resolve = async (elements: any): Promise<string> => {
 
 function escape(text: any): string {
   /* Much faster when there is no unescaped characters */
-  if (typeof text !== 'string' || !rxUnescaped.test(text)) {
+  if (typeof text !== 'string' || (text as any) instanceof Unsafe || !rxUnescaped.test(text)) {
     return text;
   }
 
@@ -64,7 +64,9 @@ export class Template extends Array {}
 
 export type TemplateType = typeof Template;
 
-export const unsafe = (value = '') => Template.from([value]);
+export class Unsafe extends String {}
+
+export const unsafe = (value = ''): string => new Unsafe([value]) as string;
 
 const SELF_CLOSING = new Set([
   'area',
@@ -147,7 +149,7 @@ const toString = (key: string, value: any) => {
       .map(entry => [kebapCache[entry[0]] || (kebapCache[entry[0]] = toKebapCase(entry[0])), entry[1]].join(':'))
       .join(';')
       .toLowerCase();
-  } else if (typeof value === 'object') {
+  } else if (typeof value === 'object' && !(value instanceof Unsafe)) {
     value = JSON.stringify(value);
   } else if (typeof value === 'boolean') {
     return ` ${keyCache[key] || (keyCache[key] = escape(key.toLowerCase()))}`;
